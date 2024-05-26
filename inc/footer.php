6a265345a989d6ab9c3c293aae51bcbeb2a678c5
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2024-05-26 22:38:07 +0700

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,32 @@
+</div>
+<div class="footer">
+   	  <div class="wrapper">	
+	     <div class="section group">
+				
+				
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>Liên hệ chúng tôi</h4>
+						<ul>
+							<li><span>ĐT:0123456789</span></li>
+							<li><span>Email:mushin12003@gmail.com</span></li>
+							<li><span>Đc:126 Nguyễn Thiện Thành, Phường 5, TP Trà Vinh </span></li>
+						</ul>
+						
+				</div>
+			</div>
+			
+     </div>
+    </div>
+    
+    <script type="text/javascript">
+		$(document).ready(function() {
+			
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+
+   
+</body>
+</php>
